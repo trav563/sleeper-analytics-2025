4b0a1753d0fcb3f21044598753efc20fd78a1470
Trav563 <trav563@gmail.com>
2025-11-25 12:43:29 -0500

    fix: Pass users prop to RivalryMatrix in HistoryPage

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -2,11 +2,11 @@ import { useOutletContext } from 'react-router-dom';
 import RivalryMatrix from '../features/analytics/components/RivalryMatrix';
 
 const HistoryPage = () => {
-    const { user } = useOutletContext();
+    const { user, users } = useOutletContext();
 
     return (
         <div className="space-y-8">
-            <RivalryMatrix currentUserId={user?.user_id} />
+            <RivalryMatrix currentUserId={user?.user_id} users={users} />
         </div>
     );
 };
